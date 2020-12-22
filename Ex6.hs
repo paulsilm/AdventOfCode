c328327569e3c855d4d566ed9d55f6efbd5cbd27
paulsilm <paul.silm99@gmail.com>
2020-12-22 18:36:27 +0200

    finished part 2 ex 6

--- Ex6.hs
+++ Ex6.hs
@@ -7,13 +7,23 @@ import Text.Regex.Posix
 az = ['a'..'z']
 
 solve :: String -> Int
-solve str = solve1 (splitOn "\n\n" str)
+solve str = solve2 (splitOn "\n\n" str)
 
 solve1 :: [String] -> Int
-solve1 answers = sum (map countAnswers answers)
+solve1 answers = sum (map countAnswers1 answers)
 
-countAnswers :: String -> Int
-countAnswers str = length (filter (\c -> elem c str) az)
+countAnswers1 :: String -> Int
+countAnswers1 str = length (filter (\c -> elem c str) az)
+
+solve2 :: [String] -> Int
+solve2 answers = sum (map countAnswers2 answers)
+
+countAnswers2 :: String -> Int
+countAnswers2 str = length (filter (\c -> elemAll c (lines str)) (filter (\c -> elem c str) az))
+
+elemAll :: Char -> [String] -> Bool
+elemAll c [] = True
+elemAll c (s:ss) = (elem c s) && elemAll c ss
 
 fieldData :: String -> String -> String
 fieldData field pass = head (words after) 
